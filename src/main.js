1d73d10887a05fb71bec5778a24f7d07b253fc4e
Faisal Rana <inquisitive@gmail.com>
2021-03-03 20:27:40 -0800

    hhad a good fail, and then a pass, initial comit
    Co-authored-by: Cat Denton <willwdenton@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import //Object from './Location.js';
+
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    ∂
+    $('#response').append("<p>" + response + "</p>");
+  });
+});
